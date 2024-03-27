36b7608d4dbeb3aa03d72f4151366f3cc7a9ae61
sldeaals <danielariasaguero@gmail.com>
2024-03-27 11:22:02 -0400

    fix: faster cities data fetching

--- src/services/city.ts
+++ src/services/city.ts
@@ -1,23 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 import { handleError } from '../utils';
+import { cities } from '../data';
 
 export interface City {
   countryCode: string;
-  postalCode: string;
-  placeName: string;
-  adminName1: string;
-  adminCode1: string;
-  adminName2: string;
-  adminCode2: string;
-  adminName3: string;
-  adminCode3: string;
-  latitude: number;
-  longitude: number;
-  accuracy: number;
+  postalCode?: string | undefined;
+  placeName?: string;
+  adminName1?: string;
+  adminCode1?: string;
+  adminName2?: string;
+  adminCode2?: string;
+  adminName3?: string;
+  adminCode3?: string;
+  latitude: number | null;
+  longitude: number | null;
+  accuracy: number | null;
 }
 
-export async function getCitiesByCountry(countryCode: string): Promise<City[] | undefined> {
+export async function getCitiesByCountryFromFile(countryCode: string): Promise<City[] | undefined> {
   try {
     const citiesJsonPath = path.join(__dirname, '../data/cities.json');
     const citiesJsonData = fs.readFileSync(citiesJsonPath, 'utf-8');
@@ -28,4 +29,8 @@ export async function getCitiesByCountry(countryCode: string): Promise<City[] | 
   } catch (error: unknown) {
     handleError(error, 'Error fetching cities');
   }
+}
+
+export function getCitiesByCountry(countryCode: string): City[] | [] {
+  return cities.filter(city => city.countryCode === countryCode);
 }
