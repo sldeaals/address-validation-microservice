6461b58f90a4f0d0e91791c866f32bf4ccd6be5d
Daniel Arias <64913142+sldeaals@users.noreply.github.com>
2024-03-31 12:20:57 -0400

    [AVS-013] - Add cluster server to project (#13)

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
-import app from './app';
+import cluster from 'cluster';
+import os from 'os';
 import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+const numCPUs = os.cpus().length;
+const desiredWorkers = Math.ceil(numCPUs / 2);
+
+if (cluster.isPrimary) {
+  console.log(`Master ${process.pid} is running`);
+
+  for (let i = 0; i < desiredWorkers; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
+} else {
+  app.listen(PORT, () => {
+    console.log(`Worker ${process.pid} started`);
+  });
+}
