a0ddbef83bb12a551621e3859e496eff85a3e4fb
sldeaals <danielariasaguero@gmail.com>
2024-04-02 15:52:13 -0400

    fix: remove read cities from file functionality

--- src/controllers/city.ts
+++ src/controllers/city.ts
@@ -1,30 +1,11 @@
 import { Request, Response } from 'express';
-import { getCitiesByCountryFromFile, getCitiesByCountry } from '../services/city';
+import { getCitiesByCountry } from '../services/city';
 import { City } from "../models";
 import {
   ApiResponse,
   sendApiResponse,
   sendErrorResponse,
 } from '../utils/apiResponse';
-
-export async function getCitiesByCountryFromFileController(
-  req: Request,
-  res: Response<ApiResponse<City[]>>
-): Promise<void> {
-  const { countryCode } = req.params;
-  try {
-    const cities = await getCitiesByCountryFromFile(countryCode);
-    sendApiResponse(res, {
-      data: cities,
-      message: 'Cities fetched successfully',
-    });
-  } catch (error) {
-    sendErrorResponse(res, {
-      error: error instanceof Error ? error.message : 'Unknown error',
-      message: 'Failed to fetch cities',
-    });
-  }
-}
 
 export async function getCitiesByCountryController(
   req: Request,
--- src/services/city.ts
+++ src/services/city.ts
@@ -1,21 +1,5 @@
-import fs from 'fs';
-import path from 'path';
-import { handleError } from '../utils';
 import { cities } from '../data';
 import { City } from '../models';
-
-export async function getCitiesByCountryFromFile(countryCode: string): Promise<City[] | undefined> {
-  try {
-    const citiesJsonPath = path.join(__dirname, '../data/cities.json');
-    const citiesJsonData = fs.readFileSync(citiesJsonPath, 'utf-8');
-    const cities: City[] = JSON.parse(citiesJsonData);
-    const filteredCities = cities.filter(city => city.countryCode === countryCode);
-
-    return filteredCities;
-  } catch (error: unknown) {
-    handleError(error, 'Error fetching cities');
-  }
-}
 
 export function getCitiesByCountry(countryCode: string): City[] | [] {
   return cities.filter(city => city.countryCode === countryCode);
