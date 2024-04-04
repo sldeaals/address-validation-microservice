52c0357c54a3406737aa40cbc0e0af77d9ad1e20
sldeaals <danielariasaguero@gmail.com>
2024-04-04 19:05:47 -0400

    feat: add test coverage to one country service handler

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -1,3 +1,4 @@
 export * from './city';
 export * from './state';
 export * from './district';
+export * from './country';
---
+++ src/services/__test__/country.test.ts
@@ -0,0 +1,30 @@
+import { Country } from '../../models';
+import { countries } from '../../mocks';
+
+describe('getCountryByCode function', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should return country data when fetched successfully', async () => {
+    const mockCountryCode = 'US';
+    const mockCountryData = countries[0];
+    const getCountryByCode = jest
+      .fn()
+      .mockResolvedValue(countries[0] as Country);
+    const countryData = await getCountryByCode(mockCountryCode);
+
+    expect(countryData).toEqual(mockCountryData);
+  });
+
+  it('should throw an error when fetching country data fails', async () => {
+    const mockCountryCode = 'DE';
+    const getCountryByCode = jest
+      .fn()
+      .mockRejectedValue(new Error('Failed to fetch country'));
+
+    await expect(getCountryByCode(mockCountryCode)).rejects.toThrow(
+      'Failed to fetch country'
+    );
+  });
+});
