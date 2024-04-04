dbb8151beb0ddb82cb6ba06d6b47831bee564b8e
sldeaals <danielariasaguero@gmail.com>
2024-04-03 20:19:12 -0400

    feat: add test coverage for utils common functions

---
+++ src/utils/__test__/common.test.ts
@@ -0,0 +1,46 @@
+import { searchByField, generateRandomString } from '../common';
+import { states } from '../../mocks';
+
+const testData = [...states];
+const searchByNameMock = [
+  {
+    countryCode: 'DO',
+    stateCode: '1',
+    name: 'Distrito Nacional',
+    cities: [
+      {
+        stateCode: 1,
+        countryCode: 'DO',
+        postalCodes: ['10101', '10102', '10103', '10110', '10111', '10112'],
+        cityId: 1,
+        name: 'Santo Domingo de Guzmán',
+      },
+    ],
+  },
+];
+
+describe('searchByField function', () => {
+  it('should return an array of objects matching the search term in the specified field (case insensitive)', () => {
+    const searchResultByName = searchByField(testData, 'name', 'Distrito Nacional');
+    expect(searchResultByName).toEqual(searchByNameMock);
+  });
+
+  it('should return an empty array if no match is found', () => {
+    const searchResult = searchByField(testData, 'name', 'Unknown');
+    expect(searchResult).toEqual([]);
+  });
+});
+
+describe('generateRandomString function', () => {
+  it('should generate a random string of the specified length', () => {
+    const length = 10;
+    const randomString = generateRandomString(length);
+    expect(randomString).toHaveLength(length);
+  });
+
+  it('should return an empty string if length is 0', () => {
+    const length = 0;
+    const randomString = generateRandomString(length);
+    expect(randomString).toBe('');
+  });
+});
